e8b66904bfc1598d6e1ddde3de5a511026174ced
Your Name <you@example.com>
2023-11-26 15:55:34 -0500

    add files

---
+++ gen_dash_from_metrics_list.py
@@ -0,0 +1,83 @@
+import random
+import string
+import os
+import pathlib
+import json
+
+
+def get_unquoted_lines(file_path):
+    with open(file_path, 'r') as file:
+        file_contents = file.read()
+    lines = file_contents.split('\n')
+    return [line.replace('"', "") for line in lines]
+
+
+def get_template_content(file_path):
+    with open(file_path, 'r') as file:
+        template_content = file.read()
+    return template_content
+
+def get_random_str(n):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=n))
+
+def gen_uid():
+    return '-'.join([
+        get_random_str(8),
+        get_random_str(4),
+        get_random_str(4),
+        get_random_str(4),
+        get_random_str(12)
+    ])
+
+
+def generate_grafana_dashboard_json(metrics_file_path):
+    metrics = get_unquoted_lines(metrics_file_path)
+    metrics = list(set(metrics))
+    job_name = pathlib.Path(metrics_file_path).stem
+
+    print(f"Job {job_name} has {len(metrics)} metrics")
+
+    template_content = get_template_content(
+        os.getenv("METRIC_TEMPLATE_FILE", "metric-template-json.json")
+    )
+    datasource = {
+        "type": "prometheus",
+        "uid": "ac7c51a0-f501-4105-89f0-b42a9dbe21d0"
+    }
+    if os.getenv("DATASOURCE"):
+        datasource = json.loads(os.getenv("DATASOURCE"))
+    metrics_list = [
+        template_content.replace(
+            "__metric__", metric
+        ).replace(
+            "__job_name__", job_name
+        ).replace(
+            "__datasource__", json.dumps(datasource)
+        ).replace("__UID__", gen_uid()) for metric in metrics
+    ]
+    metrics_text = ",".join(metrics_list)
+    dashboard_content = get_template_content(
+        os.getenv("DASH_TEMPLATE_FILE", "dash-template-json.txt")
+    )
+    dashboard = dashboard_content.replace(
+        "__datasource__", json.dumps(datasource)
+    ).replace(
+        "__title__", f"{job_name} Kafka metrics"
+    ).replace(
+        "__UID__", gen_uid()
+    ).replace(
+        "__metrics_list__", metrics_text
+    )
+    with open(f'dashboard-{job_name}.json', 'w') as file:
+        file.write(dashboard)
+
+
+if __name__ == "__main__":
+    metrics_file_paths = os.getenv("METRICS_FILES", "job.txt").split(',')
+    # metrics_file_paths = [
+    #     "kafka-broker-dispatcher-ht9sk.txt",
+    #     "kafka-broker-ingress.txt",
+    #     "kafka-sink-ingress.txt"
+    # ]
+    for metrics_file_path in metrics_file_paths:
+        generate_grafana_dashboard_json(metrics_file_path)
---
+++ readme.md
@@ -0,0 +1,61 @@
+# gen_dash_from_metrics_list.py
+
+A tool to help generate Grafana dashboards from file with list of Kafka metrics
+Tested with Python 3.11, should work with 3.9+.
+
+## How to use this tool
+
+### Create input files
+
+Create files with list of metrics, and file name should be same as "job" label, which is used for filtering.
+
+For example, file `kafka-broker-ingress.txt`:
+
+```
+kafka_app_info_start_time_ms
+kafka_producer_batch_size_avg
+kafka_producer_batch_size_max
+...
+```
+
+will generate dashboard json `dashboard-kafka-broker-ingress.json` (in same directory where script is run). This dashboard will have time series per metric name in input txt file. Each time series will be filtered with `job=kafka-broker-ingress`, job name is name of input txt file without extention.
+
+### Set env vars:
+
+```
+DATASOURCE='{"type": "prometheus","uid": "ac7c51a0-f501-4105-89f0-b42a9dbe21d0"}'
+```
+
+DATASOURCE is json of datasource to use in dashboards.
+
+```
+METRICS_FILES="kafka-broker-ingress.txt,kafka-sink-ingress.txt"
+```
+
+list of file paths - each file has metrics list in them, newline separated (like kafka-broker-ingress.txt snippet above).
+
+
+Set optional env vars:
+
+```
+DASH_TEMPLATE_FILE="dash.txt"
+```
+
+File with template of dashboard (__title__, __metrics_list__, __uid__, __metrics_list__ placeholders).
+Default dash-template-json.txt.
+
+```
+METRIC_TEMPLATE_FILE="metric.txt"
+```
+
+File with template of panel (time series), __metric__, __job_name__, __datasource__ placeholders.
+Default metric-template-json.json.
+
+
+### Run
+
+```
+>  python .\gen_dash_from_metrics_list.py
+```
+
+This will create a number of `dashboard-*.json` files in same directory.
